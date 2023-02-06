1c99731921d3bc17e356c1f6c494bc42cba8cf4b
Italo Siqueira <siqueira.italo@gmail.com>
2023-02-05 21:58:45 -0300

    add: webapi

---
+++ TesteB3.WebApi/Controllers/CdbController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TesteB3.Models;
+
+namespace TesteB3.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CdbController : Controller
+    {
+
+        [HttpPost]
+        public CdbModel Post()
+        {
+            return new CdbModel(10000, 0.9, 108, 2 );
+        }
+    }
+}
