5daf12205c9dd4607c452a8c0c7c2eef319a63c0
Italo Siqueira <siqueira.italo@gmail.com>
2023-02-05 21:06:03 -0300

    add: cdb models

---
+++ TesteB3.Models/CdbModel.cs
@@ -0,0 +1,84 @@
+﻿namespace TesteB3.Models
+{
+    public class CdbModel
+    {
+        private double cdi;
+        private double taxaBanco;
+
+
+        public CdbModel(double valorInicial, double cdi, double taxaBanco, int meses)
+        {
+            if (valorInicial <= 0)
+                throw new ArgumentException("O Valor inicial deve ser positivo");
+
+            if (meses <= 1)
+                throw new ArgumentException("a quantidade de meses deve ser maior que 1");
+
+            ValorInicial = valorInicial;
+            Cdi = cdi;
+            TaxaBanco = taxaBanco;
+            Meses = meses;
+        }
+
+
+        //VF
+        public double ValorFinal 
+        {
+            get 
+            {
+                var valorFinal = 0.0;
+                for (int i = 0; i < Meses; i++)
+                {
+                    var _ = Calculate(i == 0 ? ValorInicial : valorFinal);
+                    valorFinal = _;
+                }
+                return valorFinal;
+            }
+        }
+
+        //VI
+        public double ValorInicial { get; set; }
+
+        public double Cdi
+        {
+            get => cdi;
+            set => cdi = value / 100;
+        }
+        //TB
+        public double TaxaBanco
+        {
+            get => taxaBanco;
+            set => taxaBanco = value / 100;
+        }
+
+        public double ValorLiquido 
+        { 
+            get
+            {
+                var percentualImposto = 0.0;
+                
+                if (Meses <= 6)
+                    percentualImposto = 0.225;
+                else if (Meses > 6 && Meses <= 12)
+                    percentualImposto = 0.20;
+                else if (Meses > 12 && Meses <= 24)
+                    percentualImposto = 0.175;
+                else
+                    percentualImposto = 0.15;
+                
+                return  ValorFinal - ( Ganho * percentualImposto);
+            }
+        }
+
+        public double Ganho 
+        {
+            get => ValorFinal - ValorInicial; 
+        }
+
+        public int Meses { get; set; }
+
+
+        public double Calculate(double valor) =>
+            valor * (1 + (Cdi * TaxaBanco));
+    }
+}
---
+++ TesteB3.Tests/ModelTests.cs
@@ -0,0 +1,73 @@
+﻿using TesteB3.Models;
+
+namespace TesteB3.Tests
+{
+    /// <summary>
+    /// Testes unitarios dos calculos de CDB
+    /// calculo utilizado VF = VI x [1 + (CDI x TB)]
+    /// VF = 20000 x [ 1 + ( 0.009 * 1.08 )]
+    /// VF = 20000 x [ 1 +  0,00972 ]
+    /// VF = 20000 x 1,00972
+    /// VF = 20.194,4
+    /// o valor bruto do primeiro mes adotado para validação é 20.194.4
+    /// </summary>
+    [TestClass]
+    public class ModelTests
+    {
+
+        private readonly CdbModel _cdbModel;
+
+
+        public ModelTests()
+        {
+            _cdbModel = new CdbModel(10000, 0.9, 108, 2);
+        }
+
+        //A API deve receber um valor monetario positivo
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Um Valor inicial invalido foi aceito")]
+        public void ValorMonetarioÉPositivo()
+        {
+            var cdb = new CdbModel(0, 0.9, 108, 1);
+            Assert.IsTrue(cdb.ValorInicial == 0);
+        }
+
+        //A API deve receber um prazo em meses maior que 1 para resgate da aplicação
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Uma quantidade de meses invalida foi aceita")]
+        public void OPrazoEmMesesÉMaiorQueUm()
+        {
+            var cdb = new CdbModel(10000, 0.9, 108, 1);
+            Assert.IsTrue(cdb.Meses == 1);
+        }
+
+        [TestMethod]
+        public void oValorBrutoDoInvestimentoEstaCorreto()
+        {
+            //R$ 10.195,34
+            Assert.AreEqual(10195.34, Math.Round(_cdbModel.ValorFinal,2));
+        }
+
+        [TestMethod]
+        public void oValorLiquidoDoInvestimentoEstaCorreto()
+        {
+            // R$10.151,39
+            Assert.AreEqual(10151.39, Math.Round(_cdbModel.ValorLiquido,2));
+        }
+
+        [TestMethod]
+        public void oValorDoGanhoEstaCorreto()
+        {
+            //R$ 195.34
+            Assert.AreEqual(195.34, Math.Round(_cdbModel.Ganho,2));
+        }
+
+        [TestMethod]
+        public void oCalculoDeCdbEstaCorreto()
+        {
+            //R$ 10.097.20 
+            Assert.AreEqual(10097.20, Math.Round(_cdbModel.Calculate(10000),2));
+        }
+
+    }
+}
